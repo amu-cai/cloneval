7c2e242c30dbc9f0ed713eb944a723efbe7758ed
Iwona Christop <ichristop@g2n1.cluster.wmi.amu.edu.pl>
2024-11-25 12:27:40 +0100

    Remove unused files

--- etb/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = '0.1'
-
-from .configuration_etb import ETBConfig
-from .data_etb import ETBDataset
-from .feature_extraction_etb import ETBFeatureExtractor
-from .modeling_etb import ETBForEmotionClassification
-from .utils_etb import PREDEFINED_DATASETS
-from .wavlm import WavLM, WavLMConfig
