f4d660c8aa10498000ebbcabf3c7f1e01e6b742a
Iwona Christop <ichristop@g2n1.cluster.wmi.amu.edu.pl>
2024-11-25 12:31:55 +0100

    Restructure repository

---
+++ cloneval/cloneval.py
@@ -0,0 +1,104 @@
+import torch
+import os
+from librosa import load
+from datasets import Dataset
+import numpy as np
+
+from typing import Optional
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+from .librosa_wrapper import LibrosaWrapper
+from .wavlm import WavLM, WavLMConfig
+
+
+SAMPLING_RATE = 16_000
+
+DEVICE = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+
+class ClonEval:
+    def __init__(self) -> None:
+        self.librosa_wrapper = LibrosaWrapper(sampling_rate=SAMPLING_RATE)
+        self._init_wavlm()
+
+    def _init_wavlm(self) -> None:
+        wavlm_checkpoint = torch.load("checkpoint/WavLM-Large.pt", weights_only=True)
+        self.wavlm_config = WavLMConfig(wavlm_checkpoint["cfg"])
+        self.wavlm = WavLM(self.wavlm_config).to(DEVICE)
+        self.wavlm.load_state_dict(wavlm_checkpoint["model"])
+
+    def _read_audio(self, pth: str) -> np.ndarray:
+        waveform, _ = load(pth, sr=SAMPLING_RATE)
+        return waveform
+    
+    def _normalize_features(self, features: np.ndarray) -> np.ndarray:
+        features = np.nan_to_num(features)
+        if features.ndim < 2:
+            features = features.reshape(1, -1)
+        return features
+    
+    def _calc_cossim(self, orig_features: np.ndarray, clon_features: np.ndarray) -> float:
+        orig_features, clon_features = self._normalize_features(orig_features), self._normalize_features(clon_features)
+        cossim = np.mean(cosine_similarity(orig_features, clon_features)).item()
+        return cossim
+    
+    def _get_speaker_embeds(self, x: np.ndarray) -> torch.Tensor:
+        x = torch.tensor(x, dtype=torch.float32).view(1, -1).to(DEVICE)
+        if self.wavlm_config.normalize:
+            x = torch.nn.functional.layer_norm(x, x.shape)
+        speaker_embeds, _ = self.wavlm.extract_features(
+            x, 
+            output_layer=self.wavlm.cfg.encoder_layers, 
+            ret_layer_results=True,
+        )[0]
+        speaker_embeds = speaker_embeds.squeeze(0).detach().cpu().numpy()
+        return speaker_embeds
+
+    def eval_sample(self, orig_pth: str, clon_pth: str):
+        orig_waveform, clon_waveform = self._read_audio(orig_pth), self._read_audio(clon_pth)
+
+        if len(orig_waveform) < len(clon_waveform):
+            clon_waveform = clon_waveform[:len(orig_waveform)]
+        elif len(orig_waveform) > len(clon_waveform):
+            orig_waveform = orig_waveform[:len(clon_waveform)]
+
+        orig_features = self.librosa_wrapper(waveform=orig_waveform, sampling_rate=SAMPLING_RATE)
+        clon_features = self.librosa_wrapper(waveform=clon_waveform, sampling_rate=SAMPLING_RATE)
+
+        results = {}
+
+        for feature in orig_features:
+            orig_f = self._normalize_features(orig_features[feature])
+            clon_f = self._normalize_features(clon_features[feature])
+            cossim = self._calc_cossim(orig_f, clon_f)
+            results[feature] = cossim
+
+        results["wavlm"] = self._calc_cossim(
+            orig_features=self._get_speaker_embeds(orig_waveform),
+            clon_features=self._get_speaker_embeds(clon_waveform),
+        )
+
+        return results
+
+    def __call__(self, orig_dir: str, clon_dir: str, use_emotion: bool = False) -> None:
+        filenames = os.listdir(orig_dir)
+        results = Dataset.from_dict({"filename": filenames})
+        if use_emotion:
+            results = results.map(lambda x: {"emotion": x["filename"].replace(".wav", "").split("_")[-1]})
+        results = results.map(lambda x: self.eval_sample(orig_pth=f"{orig_dir}/{x['filename']}", clon_pth=f"{clon_dir}/{x['filename']}"))
+        if use_emotion:
+            emotions = set(results["emotion"])
+            aggregated_results = []
+            for emotion in emotions:
+                emo_results = results.filter(lambda x: x["emotion"] == emotion)
+                res = {"emotion": emotion}
+                res.update({feature: np.mean(emo_results[feature]).item() for feature in emo_results.column_names if feature not in {"emotion", "filename"}})
+                aggregated_results.append(res)
+            aggregated_results = Dataset.from_list(aggregated_results)
+        else:
+            aggregated_results = Dataset.from_dict({
+                feature: [np.mean(results[feature]).item()] for feature in results.column_names if feature not in {"emotion", "filename"}
+            })
+        results.to_csv("./results.csv")
+        aggregated_results.to_csv("./aggregated_results.csv")
---
+++ cloneval/librosa_wrapper.py
@@ -0,0 +1,91 @@
+import numpy as np
+from typing import Union
+import scipy.stats
+import torch
+import librosa
+import scipy
+
+
+class LibrosaWrapper:
+    """
+    A wrapper class to apply various Librosa transformations to audio data.
+
+    Args:
+        sampling_rate (int): The sampling rate of the input audio. Default is 16000.
+        n_fft (int): Size of FFT, creates n_fft // 2 + 1 bins. Default is 2048.
+        hop_length (int): Length of hop between STFT windows. Default is 512.
+        n_mfcc (int): Number of MFCCs to retain. Default is 13.
+    """
+    def __init__(
+        self,
+        sampling_rate: int = 16000,
+        n_fft: int = 2048,
+        hop_length: int = 512,
+        n_mfcc: int = 13,
+    ):
+        self.sampling_rate = sampling_rate
+        self.hop_length = hop_length
+        self.n_fft = n_fft
+        self.n_mfcc = n_mfcc
+
+    def __call__(self, waveform: Union[torch.Tensor, np.ndarray, list], sampling_rate: int) -> dict:
+        """
+        Process the input waveform through various Librosa transformations.
+
+        Args:
+            waveform (Union[torch.Tensor, np.ndarray, list]): The input audio waveform.
+            sampling_rate (int): The sampling rate of the input audio.
+
+        Returns:
+            dict: The processed audio features.
+        """
+        if isinstance(waveform, torch.Tensor):
+            waveform = waveform.cpu().numpy()
+        elif isinstance(waveform, list):
+            waveform = np.array(waveform)
+        
+        if sampling_rate != self.sampling_rate:
+            waveform = librosa.resample(y=waveform, orig_sr=sampling_rate, target_sr=self.sampling_rate)
+        
+        onset_env = librosa.onset.onset_strength(y=waveform, sr=self.sampling_rate)
+
+        specgram = np.abs(librosa.stft(y=waveform, hop_length=self.hop_length, n_fft=self.n_fft))
+        specgram_delta1 = librosa.feature.delta(specgram)
+
+        mel_specgram = librosa.feature.melspectrogram(S=specgram, sr=self.sampling_rate)
+        mel_specgram_delta1 = librosa.feature.delta(mel_specgram)
+
+        mfcc = librosa.feature.mfcc(y=waveform, sr=self.sampling_rate, n_mfcc=self.n_mfcc)
+        mfcc_delta1 = librosa.feature.delta(mfcc)
+
+        fmt_ac = librosa.autocorrelate(y=onset_env, max_size=10 * self.sampling_rate // self.hop_length)
+        fmt_ac = librosa.util.normalize(fmt_ac, norm=np.inf)
+        
+        return {
+            'pitch': librosa.pyin(y=waveform, sr=self.sampling_rate, fmin=65, fmax=2093)[0],
+            'spectrogram': specgram,
+            'spectrogram_delta1': specgram_delta1,
+            'mel_spectrogram': librosa.power_to_db(mel_specgram, ref=np.max),
+            'mel_spectrogram_delta1': mel_specgram_delta1,
+            'mfcc': mfcc,
+            'mfcc_delta1': mfcc_delta1,
+            'mfcc_delta2': librosa.feature.delta(mfcc_delta1),
+            'rms': librosa.feature.rms(y=waveform),
+            'spectral_centroid': librosa.feature.spectral_centroid(y=waveform, sr=self.sampling_rate, n_fft=self.n_fft, hop_length=self.hop_length),
+            'spectral_bandwidth': librosa.feature.spectral_bandwidth(y=waveform, sr=self.sampling_rate, n_fft=self.n_fft, hop_length=self.hop_length),
+            'spectral_contrast': librosa.feature.spectral_contrast(y=waveform, sr=self.sampling_rate, n_fft=self.n_fft, hop_length=self.hop_length),
+            'spectral_flatness': librosa.feature.spectral_flatness(y=waveform, n_fft=self.n_fft, hop_length=self.hop_length),
+            'spectral_rollof': librosa.feature.spectral_rolloff(y=waveform, sr=self.sampling_rate, n_fft=self.n_fft, hop_length=self.hop_length),
+            'zero_crossing_rate': librosa.feature.zero_crossing_rate(y=waveform, frame_length=self.n_fft, hop_length=self.hop_length),
+            'static_tempo': librosa.feature.tempo(onset_envelope=onset_env, sr=self.sampling_rate),
+            'static_tempo_uniform': librosa.feature.tempo(onset_envelope=onset_env, sr=self.sampling_rate, prior=scipy.stats.uniform(30, 300)),
+            'dynamic_tempo': librosa.feature.tempo(onset_envelope=onset_env, sr=self.sampling_rate, aggregate=None),
+            'dynamic_tempo_lognorm': librosa.feature.tempo(onset_envelope=onset_env, sr=self.sampling_rate, aggregate=None, prior=scipy.stats.lognorm(loc=np.log(120), scale=120, s=1)),
+            'linear_prediction_coefficients': librosa.lpc(y=waveform, order=2),
+            'tempogram': librosa.feature.tempogram(onset_envelope=onset_env, sr=self.sampling_rate, hop_length=self.hop_length),
+            'chromagram': librosa.feature.chroma_stft(y=waveform, sr=self.sampling_rate, n_fft=self.n_fft, hop_length=self.hop_length),
+            'pseudo_const_Q_transform': np.abs(librosa.pseudo_cqt(y=waveform, sr=self.sampling_rate, hop_length=self.hop_length, fmin=65, n_bins=60 * 2, bins_per_octave=12 * 2)),
+            'iirt': librosa.amplitude_to_db(np.abs(librosa.iirt(y=waveform, sr=self.sampling_rate, hop_length=self.hop_length)), ref=np.max),
+            'variable_Q_transform': librosa.amplitude_to_db(np.abs(librosa.vqt(y=waveform, sr=self.sampling_rate, hop_length=self.hop_length)), ref=np.max),
+            'const_Q_chromagram': librosa.feature.chroma_cqt(y=waveform, sr=self.sampling_rate),
+        }
